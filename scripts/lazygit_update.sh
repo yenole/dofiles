ee0f2b99f16ddde8e6e257147e2de511e389e70d
Yoruno <xusir92@gmail.com>
2024-10-16 15:28:33 +0800

    fix: yazi unzip && lazygit sudo

--- scripts/lazygit_update.sh
+++ scripts/lazygit_update.sh
@@ -3,5 +3,5 @@ VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/l
 curl -Lso /tmp/lazygit-linux.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${VERSION}_Linux_x86_64.tar.gz"
 [ -d /tmp/lazygit ] && rm -rf /tmp/lazygit
 tar zvxf /tmp/lazygit-linux.tar.gz -C /tmp/ | pv >/dev/null
-sudo install /tmp/lazygit /usr/local/bin
+install /tmp/lazygit /usr/local/bin
 echo "lazygit update v$VERSION"
--- scripts/yazi_update.sh
+++ scripts/yazi_update.sh
@@ -2,7 +2,7 @@
 VERSION=$(curl -s "https://api.github.com/repos/sxyazi/yazi/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lso /tmp/yazi-linux.zip "https://github.com/sxyazi/yazi/releases/download/v${VERSION}/yazi-x86_64-unknown-linux-gnu.zip"
 [ -d /tmp/yazi ] && rm -rf /tmp/yazi
-unzip /tmp/yazi-x86_64-unknown-linux-gnu.zip | pv >/dev/null
+unzip /tmp/yazi-linux.zip -d /tmp | pv >/dev/null
 [ -d /opt/yazi ] && rm -rf /opt/yazi
 mv /tmp/yazi-x86_64-unknown-linux-gnu /opt/yazi
 echo "yazi update v$VERSION"
