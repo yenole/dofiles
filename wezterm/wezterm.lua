3983d5fed280215821301db5e767b10831639786
Yoruno <xusir92@gmail.com>
2025-10-31 17:14:05 +0800

    feat: 更新glazewm、wezterm配置

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,32 +1,36 @@
 local wezterm = require("wezterm")
-
 local config = {
-	font = wezterm.font("Maple Mono NF"),
-	font_size = 10.5,
-	initial_cols = 130,
-	initial_rows = 30,
+    font = wezterm.font("Maple Mono NF"),
+    font_size = 10.5,
+    initial_cols = 130,
+    initial_rows = 30,
 
-	color_scheme = "Dracula",
+    color_scheme = "Dracula",
+    window_decorations = "RESIZE",
+    window_background_opacity = 0.85,
+    hide_tab_bar_if_only_one_tab = true,
+    tab_bar_at_bottom = false,
 
-	window_decorations = "RESIZE",
-	window_background_opacity = 0.85,
-	hide_tab_bar_if_only_one_tab = true,
+    default_prog = { "pwsh" },
 
-	default_prog = { "pwsh" },
+    -- 禁用默认的key绑定
+    disable_default_key_bindings = true,
+    keys = {
+        { key = "Escape",    mods = "SHIFT",     action = wezterm.action.SendKey({ key = "~" }) },
+        { key = "Delete",    mods = "SHIFT",     action = wezterm.action.SendKey({ key = "`" }) },
+        { key = "v",         mods = "ALT|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
+        { key = "UpArrow",   mods = "SHIFT",     action = wezterm.action.ScrollByLine(-1) },
+        { key = "DownArrow", mods = "SHIFT",     action = wezterm.action.ScrollByLine(1) },
+        { key = 'l',         mods = 'CTRL|SHIFT',     action = wezterm.action.ShowLauncher },
+        { key = "1",         mods = "CTRL",      action = wezterm.action.ActivateTab(0) },
+        { key = "2",         mods = "CTRL",      action = wezterm.action.ActivateTab(1) },
+        { key = "3",         mods = "CTRL",      action = wezterm.action.ActivateTab(2) },
+        { key = "4",         mods = "CTRL",      action = wezterm.action.ActivateTab(3) },
+        { key = "5",         mods = "CTRL",      action = wezterm.action.ActivateTab(4) },
+    },
 
-	-- 禁用默认的key绑定
-	disable_default_key_bindings = true,
-	keys = {
-		{ key = "Escape", mods = "SHIFT", action = wezterm.action.SendKey({ key = "~" }) },
-		{ key = "Delete", mods = "SHIFT", action = wezterm.action.SendKey({ key = "`" }) },
-		{ key = "v", mods = "ALT|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
-		{ key = "UpArrow", mods = "SHIFT", action = wezterm.action.ScrollByLine(-1) },
-		{ key = "DownArrow", mods = "SHIFT", action = wezterm.action.ScrollByLine(1) },
-	},
-
-	mouse_bindings = {
-		{ event = { Down = { streak = 1, button = "Left" } }, mods = "CTRL", action = wezterm.action.StartWindowDrag },
-	},
+    mouse_bindings = {
+        { event = { Down = { streak = 1, button = "Left" } }, mods = "CTRL", action = wezterm.action.StartWindowDrag },
+    },
 }
-
 return config
